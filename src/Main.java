fe714f8cc792fefc1fd5c29d5ded6ef6f214a554
Matdat <math03t7@stud.kea.dk>
2022-11-28 09:20:02 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("alex er sej");
+        System.out.println("alex er yt");
 
         System.out.println("Snøffel");
         System.out.println("hej");
