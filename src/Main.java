3413d3fefe14e91cc2c89efc1e7a6831d7ecc0ef
Poijus <92998600+Poijus@users.noreply.github.com>
2022-11-28 10:10:48 +0100

    Merge remote-tracking branch 'origin/master'

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("alex er sej");
 
-        System.out.println("Snøffel");
-        System.out.println("hej");
     }
 }
