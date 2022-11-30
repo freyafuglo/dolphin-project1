3a02eb4b4b23cca6c0f26b84ca94c1eb7f258b80
Matdat <math03t7@stud.kea.dk>
2022-11-30 11:11:57 +0100

    Merge remote-tracking branch 'origin/master'

--- src/MemberList.java
+++ src/MemberList.java
@@ -1,10 +1,28 @@
 import java.util.ArrayList;
 
-public class MemberList // EJERSKAB: ALEXANDER & FREYA
-{
+public class MemberList { // EJERSKAB: ALEXANDER & FREYA & MATHIAS
+
+    // ARRAYLISTS
     private ArrayList<CompetitionSwimmer> competitionSwimmers = new ArrayList<>();
     private ArrayList<CompetitionSwimmer> crawlSwimmers = new ArrayList<>();
     private ArrayList<CompetitionSwimmer> butterFlyers = new ArrayList<>();
     private ArrayList<CompetitionSwimmer> breastStrokers = new ArrayList<>();
     private ArrayList<CompetitionSwimmer> backStrokers = new ArrayList<>();
+
+    // GETTERE
+    public ArrayList<CompetitionSwimmer> getCompetitionSwimmers(){
+        return competitionSwimmers;
+    }
+    public ArrayList<CompetitionSwimmer> getCrawlSwimmers(){
+        return crawlSwimmers;
+    }
+    public ArrayList<CompetitionSwimmer> getButterFlyers(){
+        return butterFlyers;
+    }
+    public ArrayList<CompetitionSwimmer> getBreastStrokers(){
+        return breastStrokers;
+    }
+    public ArrayList<CompetitionSwimmer> getBackStrokers(){
+        return backStrokers;
+    }
 }
